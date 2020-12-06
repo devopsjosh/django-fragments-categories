7a6c857b6e10e6757872d3f822761bc9f3fa547d
Josh Tesch <jdtesch77@gmail.com>
2020-12-06 16:08:14 -0600

    Moved files

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-fragments-categories',
     version='0.1.0',
-    packages=['fragment_categories'],
+    packages=['fragment_categories', 'fragment_categories/*'],
     include_package_data=True,
     license='MIT License',
     description='Categories for the django fragments framework.',
