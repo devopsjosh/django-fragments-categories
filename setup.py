22083e63571c8b47857b926629192970934737b7
Josh Tesch <jdtesch77@gmail.com>
2020-12-06 16:55:20 -0600

    Added to manifest

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-fragment-categories',
     version='0.1.0',
-    packages=['fragment_categories'],
+    packages=find_packages(),
     license='MIT License',
     description='Categories for the django fragments framework.',
     long_description=README,
