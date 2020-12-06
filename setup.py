4c0710502c125a5b03375b2fe93fe42024f95c88
Josh Tesch <jdtesch77@gmail.com>
2020-12-06 15:47:56 -0600

    Added package to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import os
 from setuptools import setup, find_packages
- 
+
 README = open(os.path.join(os.path.dirname(__file__),
                            'README.md')).read()
- 
+
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
- 
+
 setup(
     name='django-fragments-categories',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['fragment_categories'],
     include_package_data=True,
     license='MIT License',
     description='Categories for the django fragments framework.',
