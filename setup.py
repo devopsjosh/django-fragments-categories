fa430fe061457252454baaf9defd126805b62916
Josh Tesch <jdtesch77@gmail.com>
2020-12-06 14:33:21 -0600

    Fix name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(os.path.dirname(__file__),
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
-    name='django-borg',
+    name='django-fragments-categories',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
