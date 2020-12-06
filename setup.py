29fcc58fd64a28e595dbc34d75e1d2c7f59eb78c
Josh Tesch <jdtesch77@gmail.com>
2020-12-06 17:04:11 -0600

    add pak data

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     name='django-fragment-categories',
     version='0.1.0',
     packages=find_packages(),
+    include_package_data=True,
     license='MIT License',
     description='Categories for the django fragments framework.',
     long_description=README,
